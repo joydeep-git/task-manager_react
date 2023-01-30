c7300ca454bec1e4c7a04c4a635d4b657c2b8f62
joydeep-git <joydeep.accounts@tuta.io>
2023-01-30 21:20:03 +0530

    react setup files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+
+// import COMPONENT Files
+import Task from "./Components/Task";
+import StatusLine from "./Components/StatusLine";
+
+// CSS Files
+import "./CSS/Colours.css";
+
+
+function App() {
+  return (
+    <>
+    <h3>Task Manager</h3>
+    </>
+  );
+}
+export default App;
