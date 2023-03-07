f74137e104cb6e17905c82281bb5afb29bd1d15c
Joydeep Das <joydeep.accounts@tuta.io>
2023-03-08 02:38:48 +0530

    basic change

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,76 +1,75 @@
+import React from "react";
+
 import "../CSS/task.css";
-import { useState } from "react";
 import TaskItem from "./TaskItem";
 import EditTask from "./EditTask";
+
+import { useState } from "react";
 import { db } from "../firebase/firebase";
 import { doc, updateDoc, deleteDoc } from "firebase/firestore";
+
 function Task({ id, title, description, completed }) {
+
   const [checked, setChecked] = useState(completed);
+
   const [open, setOpen] = useState({ edit: false, view: false });
 
   const handleDelete = async () => {
+
     const taskDocRef = doc(db, "tasks", id);
+
     try {
       await deleteDoc(taskDocRef);
     } catch (error) {
       alert(error);
     }
   };
+
   const handleChange = async () => {
     const taskDocRef = doc(db, "tasks", id);
+
     try {
       await updateDoc(taskDocRef, { completed: checked });
     } catch (error) {
       alert(error);
     }
   };
+
   const handleClose = () => {
     setOpen({ edit: false, view: false });
   };
+
   return (
-    <div className={`task ${checked && "task--borderColor"}`}>
+
+    <div className={`task ${checked && "task_completed"}`}>
+
       <div>
         <input
           id={`checkbox-${id}`}
-          className="checkbox-custom"
+          className="task_checkbox"
           name="checkbox"
           checked={checked}
-          onChange={handleChange}
+          onChange={() => setChecked(!checked)}
           type="checkbox"
         />
-        <label
-          htmlFor={`checkbox-${id}`}
-          className="checkbox-custom-label"
-          onClick={() => setChecked(!checked)}
-        ></label>
       </div>
-      <div className="task__body">
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <div className="task__buttons">
-          <div className="task__deleteNedit">
-            <button
-              className="task__editButton"
-              onClick={() => setOpen({ ...open, edit: true })}
-            >
-              Edit
-            </button>
-            <button className="task__deleteButton" onClick={handleDelete}>
-              Delete
-            </button>
-          </div>
-          <button
-            onClick={() =>
-              setOpen({
-                ...open,
-                view: true,
-              })
-            }
-          >
-            View
-          </button>
+
+      <div className="task_body">
+
+        <div className="task_data">
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+
+        <div className="task_buttons">
+
+          <button className="task_editButton" onClick={() => setOpen({ ...open, edit: true })}>Edit</button>
+          <button className="task_deleteButton" onClick={handleDelete}>Delete</button>
+          <button onClick={() => setOpen({ ...open, view: true, })}>View</button>
+
         </div>
       </div>
+
       {open.view && (
         <TaskItem
           onClose={handleClose}
@@ -79,6 +78,7 @@ function Task({ id, title, description, completed }) {
           open={open.view}
         />
       )}
+
       {open.edit && (
         <EditTask
           onClose={handleClose}
@@ -88,8 +88,8 @@ function Task({ id, title, description, completed }) {
           id={id}
         />
       )}
+
     </div>
   );
 }
-
 export default Task;
