2a4410987e86459e1bd1fc827dabec6841387392
Joydeep Das <99816502+joydeep-git@users.noreply.github.com>
2023-07-06 20:29:40 +0530

    changed firebase configuration

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD8nkLQuf1oYraiXpNgMjJBJeAoUW_psL8",
-    authDomain: "task-manager-react-f0a9b.firebaseapp.com",
-    projectId: "task-manager-react-f0a9b",
-    storageBucket: "task-manager-react-f0a9b.appspot.com",
-    messagingSenderId: "1009660311009",
-    appId: "1:1009660311009:web:a721c755e47655b3234ea0",
-    measurementId: "G-JFQKCBY1KP",
+  apiKey: "AIzaSyD8nkLQuf1oYraiXpNgMjJBJeAoUW_psL8",
+  authDomain: "task-manager-react-f0a9b.firebaseapp.com",
+  databaseURL: "https://task-manager-react-f0a9b-default-rtdb.firebaseio.com",
+  projectId: "task-manager-react-f0a9b",
+  storageBucket: "task-manager-react-f0a9b.appspot.com",
+  messagingSenderId: "1009660311009",
+  appId: "1:1009660311009:web:a721c755e47655b3234ea0",
+  measurementId: "G-JFQKCBY1KP"
 };
 
 const app = initializeApp(firebaseConfig);
